724ddc19fa14c60087738e42b72496c1b955a8c9
lizheng <lizheng@zhaoonline.com>
2015-08-28 18:26:18 +0800

    change defaultsimilarity

--- src/main/java/org/elasticsearch/index/similarity/DefaultSimilarityProvider.java
+++ src/main/java/org/elasticsearch/index/similarity/DefaultSimilarityProvider.java
@@ -23,8 +23,6 @@ import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexSettings;
 
 /**
  * {@link SimilarityProvider} for {@link DefaultSimilarity}.
@@ -37,7 +35,7 @@ import org.elasticsearch.index.settings.IndexSettings;
  */
 public class DefaultSimilarityProvider extends AbstractSimilarityProvider {
 
-    private final DefaultSimilarity similarity = new DefaultSimilarity();
+    private final DefaultSimilarity similarity = new OverrideSimilarity();
 
     @Inject
     public DefaultSimilarityProvider(@Assisted String name, @Assisted Settings settings) {
